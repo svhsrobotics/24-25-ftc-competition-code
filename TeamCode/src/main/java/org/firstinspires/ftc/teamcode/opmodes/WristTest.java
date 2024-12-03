69777074d2ed1e04a0fae70ecbf3abd19299f0c0
ExultantExalted <pakzadryan@gmail.com>
2024-12-02 20:52:45 -0500

    WHY DO SERVOS HATE ME (attempted claw control-called WristTest)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/WristTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/WristTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @Config
@@ -17,14 +18,16 @@ public class WristTest extends LinearOpMode {
         Servo SomethingServo;
         SomethingServo = hardwareMap.get(Servo.class, "CHANGETHISORURSTUPID");
 
+
         while (opModeIsActive()) {
             if(gamepad1.right_bumper){
-                SomethingServo.setPosition(0.9);
+                SomethingServo.setPosition(0.1);
             }
             else {
                 SomethingServo.setPosition(0);
 
             }
+
             telemetry.addData("servo pos", SomethingServo.getPosition());
             telemetry.addData("bumper?", gamepad1.right_bumper);
             telemetry.addData("bumper2?", gamepad1.left_bumper);
