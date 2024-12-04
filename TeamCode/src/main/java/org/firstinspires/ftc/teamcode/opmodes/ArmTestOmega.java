af85412f723a1e3440fe9964144780c3f001903e
truman.eustace <true92410@gmail.com>
2024-12-04 18:19:41 -0500

    arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ArmTestOmega.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ArmTestOmega.java
@@ -9,6 +9,7 @@ public class ArmTestOmega extends LinearOpMode {
     private DcMotor arm = hardwareMap.get(DcMotor.class,"ArmOrwhateverr");
 
     public void runOpMode() throws InterruptedException {
+        waitForStart();
         while (opModeIsActive()){
             if (gamepad1.right_stick_y>0){
                 arm.setPower(1);
