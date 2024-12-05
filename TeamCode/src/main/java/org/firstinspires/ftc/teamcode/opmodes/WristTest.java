256f188d697c9ed13833071f7030acf369acbbc2
gavin.laslo <gavin.laslo@student.svpanthers.org>
2024-12-04 19:48:17 -0500

    wrist test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/WristTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/WristTest.java
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
-
+@Config
+@TeleOp
 public class WristTest extends LinearOpMode {
 
     public void runOpmode() throws InterruptedException{
@@ -11,9 +14,16 @@ public class WristTest extends LinearOpMode {
         Servo wrist;
         double servoPos = 0.5;
         wrist =hardwareMap.get(Servo.class, "wrist");
-        if (gamepad2.left_stick_x > 0.5){
+
+        while (opModeIsActive()){
+        if (gamepad2.left_bumper){
             wrist.setPosition(servoPos);
-            servoPos = servoPos = 0.1;
+            servoPos = servoPos - .1;
+
+        }
+        if (gamepad2.right_bumper){
+            wrist.setPosition(servoPos);
+            servoPos = servoPos + .1;
 
         }
 
