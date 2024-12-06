c2d1327f1f41ff7ccb1e3f56f977db790736b497
truman.eustace <true92410@gmail.com>
2024-12-05 19:30:43 -0500

    made a basic gamma drive mode we can add on later

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GammaDriveMode.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class GammaDriveMode extends LinearOpMode {
+    private DcMotor topLeftMotor;
+    private DcMotor topRightMotor;
+    private DcMotor bottomLeftMotor;
+    private DcMotor bottomRightMotor;
+    public void runOpMode() throws InterruptedException {
+        topLeftMotor = hardwareMap.get(DcMotor.class,"leftFront");
+        topRightMotor = hardwareMap.get(DcMotor.class,"rightFront");
+        bottomLeftMotor = hardwareMap.get(DcMotor.class,"leftBack");
+        bottomRightMotor = hardwareMap.get(DcMotor.class,"rightBack");
+        topLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        bottomLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        waitForStart();
+        double y = -gamepad1.left_stick_y; // Y stick is reversed!
+        double x = gamepad1.left_stick_x;
+        double rx2 = gamepad1.right_stick_x;
+
+        topLeftMotor.setPower(y+ x + rx2);
+        bottomLeftMotor.setPower(y - x + rx2);
+        topRightMotor.setPower(y - x - rx2);
+        bottomRightMotor.setPower(y + x - rx2);
+    }
+}
