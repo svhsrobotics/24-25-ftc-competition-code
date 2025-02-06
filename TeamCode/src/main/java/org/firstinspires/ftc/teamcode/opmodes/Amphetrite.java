fcc4bf48fb111684ea029e91373538c67a14d48c
brennan.nepomuceno <brennannepomuceno@gmail.com>
2025-02-05 19:04:17 -0500

    oh no something happened to gavin's computer so i should probbably commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Amphetrite.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Amphetrite.java
@@ -35,11 +35,19 @@ public class Amphetrite extends LinearOpMode{
     outSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     outSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     outSlide.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        outSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        outSlide.setTargetPosition(0);
+
     //
     upSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     upSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     upSlide.setDirection(DcMotorSimple.Direction.REVERSE);
-    rightBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    //non reversed wheels
+    rightFrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+     leftFrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+     leftBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
 
     waitForStart();
 
@@ -47,19 +55,29 @@ public class Amphetrite extends LinearOpMode{
        slidetToggle.update(gamepad1.left_stick_button);
        //out slide
         if(gamepad1.left_trigger != 0 && !slidetToggle.state){
-            outReference = outReference - 10 * (double) gamepad1.left_trigger;
+           // outReference = outReference - 10 * (double) gamepad1.left_trigger;
+            outSlide.setTargetPosition(outSlide.getCurrentPosition() - 1000* (int) gamepad1.left_trigger);
         }
         else if(gamepad1.right_trigger != 0 && !slidetToggle.state){
-            outReference = outReference + 10* (double) gamepad1.right_trigger;
+            //outReference = outReference + 10* (double) gamepad1.right_trigger;
+            outSlide.setTargetPosition(outSlide.getCurrentPosition() + 1000* (int) gamepad1.right_trigger);
         }
-        if (outReference >= 4890){
+
+        if(outSlide.getTargetPosition() >= 3000){
+            outSlide.setTargetPosition(3000);
+        }
+        else if(outSlide.getTargetPosition() <=0){
+            outSlide.setTargetPosition(0);
+        }
+        /*if (outReference >= 4890){
             outReference = 4890;}
         if(outReference <= 0){
-            outReference = 0;}
+            outReference = 0;}*/
         if(upReference <=0){
             upReference =0;
         }
-        outSlide.setPower(PidV.usePIDLoop(outSlide.getCurrentPosition(), outReference));
+        outSlide.setPower(1);
+        //outSlide.setPower(PidV.usePIDLoop(outSlide.getCurrentPosition(), outReference));
         //up slide
         if(gamepad1.left_trigger != 0 && slidetToggle.state){
             upReference = upReference - 10 * (double) gamepad1.left_trigger;
@@ -67,18 +85,51 @@ public class Amphetrite extends LinearOpMode{
         else if(gamepad1.right_trigger != 0 && slidetToggle.state){
             upReference = upReference + 10* (double) gamepad1.right_trigger;
         }
-        if (upReference >= 3000){
-            upReference = 3000;}
-        upSlide.setPower(PidH.usePIDLoop(upSlide.getCurrentPosition(), upReference));
+        if (upReference >= 5000){
+            upReference = 5000;}
+        upSlide.setPower(PidV.usePIDLoop(upSlide.getCurrentPosition(), upReference));
         //out claw stuff
-        outClawRotationServo.setPosition(1);
 
-        if(gamepad1.left_bumper){
-           outServo.setPosition(1);
+
+        if(gamepad1.left_bumper && !slidetToggle.state){
+           outServo.setPosition(0.5);
         }
-        else if(gamepad1.right_bumper){
+        else if(gamepad1.right_bumper && !slidetToggle.state){
             outServo.setPosition(0);
         }
+        if(gamepad1.dpad_left && !slidetToggle.state){
+            outClawRotationServo. setPosition(outClawRotationServo.getPosition()-0.01);
+        }
+        else if(gamepad1.dpad_right && !slidetToggle.state){
+            outClawRotationServo.setPosition(outClawRotationServo.getPosition() +0.01);
+        }
+        //outClawRotationServo.setPosition(outClawRotationServo.getPosition());
+        //up servos
+        //TODO:MAKE THE GODDAMN PASSTHROUGH
+
+        if(gamepad1.left_bumper && slidetToggle.state){
+           upClaw.setPosition(0);
+        }
+        else if(gamepad1.right_bumper && slidetToggle.state){
+            upClaw.setPosition(1);
+        }
+        if(gamepad1.dpad_left && slidetToggle.state){
+            upViperSlideArm. setPosition(upViperSlideArm.getPosition() - 0.01);
+        }
+        else if(gamepad1.dpad_right && slidetToggle.state){
+            upViperSlideArm. setPosition(upViperSlideArm.getPosition() + 0.01);
+        }
+        //passthrough! :P
+        if(gamepad1.a){
+            outSlide.setTargetPosition(1061);
+            //outServo.setPosition(0.5);
+            upReference = 0;
+            upViperSlideArm.setPosition(0);
+            outClawRotationServo.setPosition(1);
+//TODO god please don't make me retune that pid loop
+        }
+
+
         //drive
         double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
         double x = gamepad1.left_stick_x;
@@ -93,6 +144,9 @@ public class Amphetrite extends LinearOpMode{
         telemetry.addData("up reference", upReference);
         telemetry.addData("left trigger", gamepad1.left_trigger);
         telemetry.addData("right trigger", gamepad1.right_trigger);
+        telemetry.addData("out pos", outSlide.getCurrentPosition());
+        telemetry.addData("up Pos", upSlide.getCurrentPosition());
+        telemetry.addData("arm pos", upViperSlideArm.getPosition());
         telemetry.update();
 
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoAttempt1.java
@@ -0,0 +1,101 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.util.PIDController2;
+
+@Config
+@Autonomous(name = "INeedToNameThisSomething", group = "Autonomous" )
+public class AutoAttempt1 extends LinearOpMode {
+    public class Lift {
+        private DcMotor upSlide;
+
+        public Lift(HardwareMap hardwareMap) {
+
+            upSlide = hardwareMap.get(DcMotor.class, "notheight");
+            upSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            upSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            upSlide.setDirection(DcMotorSimple.Direction.REVERSE);
+        }
+        public class highBasket implements Action {
+            private PIDController2 PID = new PIDController2(1.2 * 0.02 / 1.28, 0.02 * 0.6, 0.075 * 0.02 * 1.28, 1);
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                upSlide.setPower(PID.usePIDLoop(upSlide.getCurrentPosition(), 4000));
+                if (upSlide.getCurrentPosition() == 4000) {
+                    return false;
+                } else {
+                    return true;
+                }
+
+
+            }
+        }
+        public class goToZero implements  Action{
+           public boolean run(@NonNull TelemetryPacket packet){
+
+               if(upSlide.getCurrentPosition() == 0){
+                   return false;
+               }
+               else{
+                   return true;
+               }
+           }
+
+    }
+    public class Out{
+        private DcMotor outSlide;
+        public Out(HardwareMap hardwareMap){
+            outSlide =  hardwareMap.get(DcMotor.class, "out");
+            outSlide.setDirection(DcMotorSimple.Direction.REVERSE);
+        }
+    }
+    public class MiniArm {
+        private Servo arm;
+        public MiniArm(HardwareMap hardwareMap){
+            arm = hardwareMap.get(Servo.class, "arm");
+        }
+    }
+
+    public class upClaw{
+        private Servo verticalClaw;
+        public upClaw(HardwareMap hardwaremap){
+            verticalClaw = hardwaremap.get(Servo.class, "upClaw");
+        }
+    }
+
+    public class outclaw{
+        private Servo beetleLol;
+        public outclaw(HardwareMap hardwareMap){
+            beetleLol = hardwareMap.get(Servo.class, "outClawServo");
+        }
+    }
+    public class outClawRotation{
+        private Servo outClawRotate;
+        public outClawRotation(HardwareMap hardwareMap){
+            outClawRotate = hardwareMap.get(Servo.class, "outClawRotationServo");
+        }
+    }
+
+
+
+    }
+
+}
+
+
+
+
+
+
