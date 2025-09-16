f9db9c4d2af53314975902fd462bcd57245752b3
brennan.nepomuceno <brennannepomuceno@gmail.com>
2025-09-16 14:31:03 -0400

    WHY IS EVERYTHING BROKEN

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SomethingMoreDistinct.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.util.AimAssist;
+@TeleOp(name="HIIIIIIIIIIIII")
+public class SomethingMoreDistinct extends LinearOpMode {
+     AimAssist gun = new AimAssist();
+private double angle = 30;
+private double xDistance = 130;
+private double yDistance = 30.9;
+private double power= 0;
+//power should should be 50
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+
+        while(opModeIsActive()){
+          power = gun.run(angle,xDistance, yDistance);
+          telemetry.addData("result", power);
+          telemetry.update();
+          //todo fix it
+
+
+
+
+        }
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/AimAssist.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.util;
+
+/***
+ * angle: the angle the gun is aimed at
+ * xDistance: the horizontal distance from the robot to the target
+ * yDistance: the vertical distance between the robot and the target
+ */
+public class AimAssist {
+    public double run(double angle, double xDistance, double yDistance ) {
+        double power =
+                Math.sqrt(
+                        4.9*Math.pow(xDistance/Math.cos(angle), 2)
+                        /
+                                xDistance * Math.tan(angle) - yDistance
+
+                )
+
+
+                ;
+
+        return(power);
+
+
+    }
+
+}
