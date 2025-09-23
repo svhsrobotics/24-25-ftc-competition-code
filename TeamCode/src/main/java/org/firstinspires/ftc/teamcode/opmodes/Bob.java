017a618e8c3ffb991c3f99b6a793bfce12db1a63
haisen.mei <haisen.mei@student.svpanthers.org>
2025-09-22 20:13:31 -0400

    did something!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Bob.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class Bob extends OpMode{
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "World");
+    }
+    @Override
+    public void loop() {
+
+    }
+}
