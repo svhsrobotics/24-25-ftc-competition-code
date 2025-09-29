b71f63abb69778d1e04c9f2923ee21c5b46cb8d1
haisen.mei <haisen.mei@student.svpanthers.org>
2025-09-29 18:37:12 -0400

    did something!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Bob.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Bob.java
@@ -3,12 +3,20 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import java.util.ArrayList;
+
 @TeleOp
 public class Bob extends OpMode{
 
+    ArrayList<String> bobStuff = new ArrayList<String>();
+
+
     @Override
     public void init() {
-
+        bobStuff.add("Cool");
+        bobStuff.add("Hi");
+        bobStuff.add("This game is fum!");
+        bobStuff.set(2, "THIS GAME IS WIGGED!!!!!!!!!!");
     }
 
     @Override
