ea51f70110475f54224c0d32095a3549bb87e014
brennan.nepomuceno <brennannepomuceno@gmail.com>
2025-09-29 20:41:32 -0400

    checkpoint

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoAttemptTheFourthHolyImports.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoAttemptTheFourthHolyImports.java
@@ -4,11 +4,15 @@ import static org.firstinspires.ftc.teamcode.SparkFunOTOSDrive.NewDrive;
 
 import androidx.annotation.NonNull;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -16,7 +20,8 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.SparkFunOTOSDrive;
-
+@Config
+@Autonomous
 public class AutoAttemptTheFourthHolyImports extends LinearOpMode {
 
 
@@ -61,8 +66,14 @@ public class AutoAttemptTheFourthHolyImports extends LinearOpMode {
         Pose2d startPose = new Pose2d(0, 0, 0);
         SparkFunOTOSDrive drive = NewDrive(hardwareMap, startPose);
         TrajectoryActionBuilder steve = drive.actionBuilder(startPose)
-                .strafeTo(new Vector2d(-60, 60));
-
+                .strafeTo(new Vector2d(-30, 30));
+        Action trajectoryActionCloseout = steve.endTrajectory().fresh().build();
+        waitForStart();
+        if(isStopRequested()) return;
+        Actions.runBlocking(
+                new SequentialAction(steve.build(),
+                        launcher.maxLaucherSpeed(),
+                        trajectoryActionCloseout));
     }
 
 
