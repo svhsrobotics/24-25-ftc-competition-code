e514986ea8afe5b3eb4321be5831a512aebf18cf
haisen.mei <haisen.mei@student.svpanthers.org>
2025-10-01 20:22:29 -0400

    drove around! (but badly)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Bob.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Bob.java
@@ -4,8 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import java.util.ArrayList;
-
 @TeleOp
 public class Bob extends OpMode{
 
