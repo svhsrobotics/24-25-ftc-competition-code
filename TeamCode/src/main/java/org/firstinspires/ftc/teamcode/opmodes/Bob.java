9b7610a386d183e8a42af056893b91c3ca602c2e
haisen.mei <haisen.mei@student.svpanthers.org>
2025-10-07 17:47:56 -0400

    strafing and joystick

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Bob.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Bob.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class Bob extends OpMode{
@@ -15,6 +16,9 @@ public class Bob extends OpMode{
     DcMotorEx side;
     DcMotorEx leftEx;
     DcMotorEx rightEx;
+    int driving;
+    double motorPower;
+    double motorTurn;
 
     @Override
     public void init(){
@@ -25,15 +29,71 @@ public class Bob extends OpMode{
         side = hardwareMap.get(DcMotorEx.class, "side_ex");
         leftEx = hardwareMap.get(DcMotorEx.class, "left_ex");
         rightEx = hardwareMap.get(DcMotorEx.class, "right_ex");
+        driving = 1;
     }
 
     @Override
     public void loop() {
-        double motorPower = -gamepad1.right_stick_y * (1 - gamepad1.right_trigger);
-        double motorTurn = -gamepad1.right_stick_x * (1 - gamepad1.right_trigger);
-        leftFront.setPower(-motorPower + motorTurn);
-        leftBack.setPower(-motorPower + motorTurn);
-        rightFront.setPower(motorPower + motorTurn);
-        rightBack.setPower(motorPower + motorTurn);
+
+        telemetry.addData("Drivemode: ", driving);
+
+        if (gamepad1.b) {
+            if (driving == 1) {
+                driving = 2;
+            }
+            else {
+                driving = 1;
+            }
+        }
+
+        if (driving == 1) {
+            motorPower = -gamepad1.left_stick_y * (1 - gamepad1.right_trigger);
+            motorTurn = -gamepad1.right_stick_x * (1 - gamepad1.right_trigger);
+            leftFront.setDirection(DcMotor.Direction.FORWARD);
+            rightBack.setDirection(DcMotor.Direction.FORWARD);
+            leftBack.setDirection(DcMotor.Direction.FORWARD);
+            rightFront.setDirection(DcMotor.Direction.FORWARD);
+            leftFront.setPower(-motorPower + motorTurn);
+            leftBack.setPower(-motorPower + motorTurn);
+            rightFront.setPower(motorPower + motorTurn);
+            rightBack.setPower(motorPower + motorTurn);
+        }
+        else {
+            if (gamepad1.dpad_up) {
+                leftFront.setDirection(DcMotor.Direction.FORWARD);
+                rightBack.setDirection(DcMotor.Direction.FORWARD);
+                leftBack.setDirection(DcMotor.Direction.FORWARD);
+                rightFront.setDirection(DcMotor.Direction.FORWARD);
+                motorPower = gamepad1.right_trigger;
+           }
+            else if (gamepad1.dpad_down) {
+                leftFront.setDirection(DcMotor.Direction.FORWARD);
+                rightBack.setDirection(DcMotor.Direction.FORWARD);
+                leftBack.setDirection(DcMotor.Direction.FORWARD);
+                rightFront.setDirection(DcMotor.Direction.FORWARD);
+                motorPower = -gamepad1.right_trigger;
+            }
+            else if (gamepad1.dpad_left) {
+                leftFront.setDirection(DcMotor.Direction.REVERSE);
+                rightBack.setDirection(DcMotor.Direction.REVERSE);
+                leftBack.setDirection(DcMotor.Direction.FORWARD);
+                rightFront.setDirection(DcMotor.Direction.FORWARD);
+                motorPower = gamepad1.right_trigger;
+            }
+            else if (gamepad1.dpad_right) {
+                leftFront.setDirection(DcMotor.Direction.FORWARD);
+                rightBack.setDirection(DcMotor.Direction.FORWARD);
+                leftBack.setDirection(DcMotor.Direction.REVERSE);
+                rightFront.setDirection(DcMotor.Direction.REVERSE);
+                motorPower = gamepad1.right_trigger;
+            }
+            else {
+                motorPower =0;
+            }
+            leftFront.setPower(-motorPower);
+            leftBack.setPower(-motorPower);
+            rightFront.setPower(motorPower);
+            rightBack.setPower(motorPower);
+        }
     }
 }
