f42fc8474ef70abfe7708cef175416bec86123ac
haisen.mei <haisen.mei@student.svpanthers.org>
2025-10-07 16:24:09 -0400

    drove around with different wheels!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Bob.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Bob.java
@@ -12,6 +12,9 @@ public class Bob extends OpMode{
     DcMotorEx leftBack;
     DcMotorEx rightFront;
     DcMotorEx rightBack;
+    DcMotorEx side;
+    DcMotorEx leftEx;
+    DcMotorEx rightEx;
 
     @Override
     public void init(){
@@ -19,12 +22,15 @@ public class Bob extends OpMode{
         leftBack = hardwareMap.get(DcMotorEx.class, "left_back");
         rightFront = hardwareMap.get(DcMotorEx.class, "right_front");
         rightBack = hardwareMap.get(DcMotorEx.class, "right_back");
+        side = hardwareMap.get(DcMotorEx.class, "side_ex");
+        leftEx = hardwareMap.get(DcMotorEx.class, "left_ex");
+        rightEx = hardwareMap.get(DcMotorEx.class, "right_ex");
     }
 
     @Override
     public void loop() {
-        double motorPower = -gamepad1.right_stick_y;
-        double motorTurn = -2 * gamepad1.right_stick_x;
+        double motorPower = -gamepad1.right_stick_y * (1 - gamepad1.right_trigger);
+        double motorTurn = -gamepad1.right_stick_x * (1 - gamepad1.right_trigger);
         leftFront.setPower(-motorPower + motorTurn);
         leftBack.setPower(-motorPower + motorTurn);
         rightFront.setPower(motorPower + motorTurn);
