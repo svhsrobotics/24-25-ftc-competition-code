e1b7ec5d36d5ae47e5ba0f2084d30ac8536b6900
brennan.nepomuceno <brennannepomuceno@gmail.com>
2025-10-21 15:01:43 -0400

    colins stuff teach him git next time

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ColinsOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ColinsOpmode.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import java.util.random.RandomGenerator;
 
@@ -10,14 +11,24 @@ import kotlin.random.Random;
 
 @TeleOp
 
+/// random notes pls ignore ///
+
+/// micah prefers shooting end is front ///
+/// mr svit says to rename the wheels whatever you want ///
+///  when uploading/pushing code remember to not connect to other teams robot lol ///
+
+
 public class ColinsOpmode extends OpMode {
     @Override
-    public void init() {
 
+    private DcMotor frontLeftWheel;
+    public void init() {
+        frontLeftWheel = hardwareMap.get(DcMotor.class, "FLWheel");
+        frontRightWheel = hardwareMap.get(DcMotor.class, "FRWheel");
     }
     @Override
     public void loop() {
-        System.out.println("wow isn't this opmode so cool"); // wow im so proud of myself
+
     }
 
 }
