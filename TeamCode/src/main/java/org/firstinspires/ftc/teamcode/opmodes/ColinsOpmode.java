a8fb76cef022af0686610b8a091a2c00748d8014
brennan.nepomuceno <brennannepomuceno@gmail.com>
2025-10-21 15:56:10 -0400

    colins stuff teach him git next time

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ColinsOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ColinsOpmode.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import java.util.random.RandomGenerator;
 
@@ -21,17 +22,32 @@ import kotlin.random.Random;
 public class ColinsOpmode extends OpMode {
     private DcMotor frontLeftWheel;
     private DcMotor frontRightWheel;
+    private DcMotor backLeftWheel;
+    private DcMotor backRightWheel;
 
     @Override
 
     public void init() {
-        frontLeftWheel = hardwareMap.get(DcMotor.class, "FLWheel");
-        frontRightWheel = hardwareMap.get(DcMotor.class, "FRWheel");
+        frontLeftWheel = hardwareMap.get(DcMotor.class, "leftFront");
+        frontRightWheel = hardwareMap.get(DcMotor.class, "rightFront");
+        backLeftWheel = hardwareMap.get(DcMotor.class, "leftBack");
+        backRightWheel = hardwareMap.get(DcMotor.class, "rightBack");
+
+        frontLeftWheel.setDirection(DcMotor.Direction.REVERSE);
+        frontRightWheel.setDirection(DcMotor.Direction.FORWARD);
+        backLeftWheel.setDirection(DcMotor.Direction.REVERSE);
+        backRightWheel.setDirection(DcMotor.Direction.FORWARD);
     }
     @Override
     public void loop() {
-        frontLeftWheel.setPower(gamepad1.left_stick_y);
-        frontRightWheel.setPower(gamepad1.left_stick_y);
+        double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
+
+        frontLeftWheel.setPower(y + x + rx);
+        backLeftWheel.setPower(y - x + rx);
+        frontRightWheel.setPower(y - x - rx);
+        backRightWheel.setPower(y + x - rx);
     }
 
 }
