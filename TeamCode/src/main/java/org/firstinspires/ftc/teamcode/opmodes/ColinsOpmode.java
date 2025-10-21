25d7259eddad77081bbc3e8c0097e5ba05450cc5
brennan.nepomuceno <brennannepomuceno@gmail.com>
2025-10-20 20:44:51 -0400

    colins stuff teach him git next time

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ColinsOpmode.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import java.util.random.RandomGenerator;
+
+import kotlin.random.Random;
+
+@TeleOp
+
+public class ColinsOpmode extends OpMode {
+    @Override
+    public void init() {
+
+    }
+    @Override
+    public void loop() {
+        System.out.println("wow isn't this opmode so cool"); // wow im so proud of myself
+    }
+
+}
