e3bb8cfb3bad08f8d9b4b701f8991991ca65ad0d
liam <cpwzgkv757@privaterelay.appleid.com>
2025-12-18 15:10:48 -0500

    better shoot macro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/IntoTheDeep/FINALTELEOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/IntoTheDeep/FINALTELEOP.java
@@ -132,7 +132,7 @@ gateServo2.setDirection(Servo.Direction.REVERSE);
                     right.setPower(0);
                     left.setPower(0);
                     intake.setPower(0.2);
-                    sleep(2000);
+                    sleep(6000);
                     gateServo.setPosition(0.48);
                     gateServo2.setPosition(0.48);
                     sleep(100);
