8ecaaa7b85d87a202807fe345fdb32725b639158
haisen.mei <haisen.mei@student.svpanthers.org>
2025-12-18 17:39:49 -0500

    untested camera test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BobTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BobTest.java
@@ -1,45 +1,68 @@
 package org.firstinspires.ftc.teamcode;
 
+import static java.lang.String.format;
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @TeleOp
-public class BobTest extends OpMode {
+public class BobTest extends LinearOpMode {
 
-    DcMotor leftFront;
-    DcMotor leftBack;
-    DcMotor rightFront;
-    DcMotor rightBack;
-    double shoot;
-    double y;
-    double x;
-    double rx;
+    AprilTagProcessor tagProcessor;
+    VisionPortal visionPortal;
+    List<AprilTagDetection> witnessedTags = new ArrayList<>();
+    Telemetry tagInfo;
+    double detections;
 
-    @Override
-    public void init() {
-        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
-        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
-        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
-        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
-        leftFront.setDirection(DcMotor.Direction.REVERSE);
-        rightBack.setDirection(DcMotor.Direction.FORWARD);
-        leftBack.setDirection(DcMotor.Direction.REVERSE);
-        rightFront.setDirection(DcMotor.Direction.FORWARD);
+    public void init(HardwareMap hwMap, Telemetry tagInfo) {
+        this.tagInfo = tagInfo;
+        tagProcessor = new AprilTagProcessor.Builder()
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setOutputUnits(DistanceUnit.CM, AngleUnit.DEGREES)
+                .build();
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        builder.setCamera(hwMap.get(WebcamName.class, "Webcam 1"));
+        builder.setCameraResolution(new Size(640, 480));
+        builder.addProcessor(tagProcessor);
+
+        visionPortal = builder.build();
     }
 
     @Override
-    public void loop () {
+    public void runOpMode() throws InterruptedException {
 
-        y = -gamepad1.left_stick_y;
-        x = gamepad1.left_stick_x;
-        rx = gamepad1.right_stick_x;
-        //backwards for LSY
-        //leftFront.setPower(0.5 * (y + x + rx));
-        leftBack.setPower(0.5 * (y - x + rx));
-        //rightFront.setPower(0.5 * (y - x - rx));
-        //rightBack.setPower(0.5 * (y + x - rx));
+        witnessedTags = tagProcessor.getDetections();
+
+        for(AprilTagDetection detection : witnessedTags) {
+            if(detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6, 1f, %6, 1f, %6, 1f, (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6, 1f, %6, 1f, %6, 1f, (degree)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+            }
+            else {
+                telemetry.addData("Unknown ", detection.id);
+            }
+        }
     }
 }
