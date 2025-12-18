768b6365ddccf9fa5e08a447dd11464231ab95b6
liam <cpwzgkv757@privaterelay.appleid.com>
2025-12-18 14:46:16 -0500

    brennan your voltage thing doesnt work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/IntoTheDeep/FINALTELEOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/IntoTheDeep/FINALTELEOP.java
@@ -141,16 +141,16 @@ gateServo2.setDirection(Servo.Direction.REVERSE);
             }
 
 
-            if ((intake.getCurrent(CurrentUnit.MILLIAMPS) > averageCurrent * 3) ){
+           // if ((intake.getCurrent(CurrentUnit.MILLIAMPS) > averageCurrent * 3) ){
                 frontLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.ORANGE);
                 rearLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.ORANGE);
-            } else if(intake.getCurrent(CurrentUnit.MILLIAMPS) > 5) {
+           // } else if(intake.getCurrent(CurrentUnit.MILLIAMPS) > 5) {
                 totalCurrent += intake.getCurrent(CurrentUnit.MILLIAMPS);
                 denominator += 1;
                 averageCurrent = totalCurrent/denominator;
             }
 
-            if (voltSensor.getVoltage() < 11.5) {
+           // if (voltSensor.getVoltage() < 11.5) {
                 telemetry.addLine("YOUR VOLTAGE IS LOW");
                 telemetry.update();
             }
